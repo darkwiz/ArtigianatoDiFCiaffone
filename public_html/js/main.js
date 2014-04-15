5afaceff7f117462e1604d947359b7e33e4129e2
darkwiz <ilbaroncino@gmail.com>
2014-04-15 23:41:53 +0200

    Code Cleanup

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 var DisplayProduct =
 {       
         mySelect: undefined,
@@ -73,7 +66,6 @@ $(function() {
      $('#menu').slicknav({
 		prependTo:'#mobile_menu'
     });
-    
    $(DisplayProduct.myConfig.product).click( function(event){
        DisplayProduct.mySelect = $(this);
        DisplayProduct.getProduct(event)
